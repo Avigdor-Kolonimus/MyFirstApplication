b244f87d4b9dd756084d968ce584797f2f36b427
LittleRedRidingHood <kolonimys@gmail.com>
2018-11-04 19:20:20 +0200

    permission

--- app/src/main/java/com/littleredridinghood/firstassignment/MainActivity.java
+++ app/src/main/java/com/littleredridinghood/firstassignment/MainActivity.java
@@ -15,6 +15,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.Manifest;
+
 
 public class MainActivity extends AppCompatActivity{
     //global variables
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity{
     private EditText etName;
     private String NameOfUser;
     private Intent intent;
+    private boolean permission=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +39,21 @@ public class MainActivity extends AppCompatActivity{
         NameOfUser="";
         etName = (EditText) findViewById(R.id.nameMain);
         btCamera = (Button) findViewById(R.id.buttonMain);
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, Image_Capture_Code);
         btCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cInt,Image_Capture_Code);
+                if (permission) {
+                    Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cInt, Image_Capture_Code);
+                }else{
+                    finish();
+                    startActivity(getIntent());
+                }
             }
         });
     }
+    //open next page
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == Image_Capture_Code) {
@@ -53,6 +68,29 @@ public class MainActivity extends AppCompatActivity{
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             }
+        }
+    }
+    //alert and question about permission
+    @Override
+    public void onRequestPermissionsResult(int request, @NonNull String[] permissions, @NonNull int[] grantResults){
+        switch(request){
+            case Image_Capture_Code:
+                if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    permission=true;
+                }
+                else {
+                    AlertDialog.Builder alert1 = new AlertDialog.Builder(this);
+                    alert1.setMessage("My friend, you must allow to use the app.");
+                    alert1.setCancelable(true);
+                    alert1.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog alert = alert1.create();
+                    alert.show();
+                    permission=false;
+                }
         }
     }
 }
